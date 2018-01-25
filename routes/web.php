6e814cb28ab44b2c9e82fc5fd59e925ac107bc1c
telton <telton@umflint.edu>
2018-01-25 14:43:33 -0500

    Changed home route to use HomeController.
    Not just return a view.

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [
+    'as'   => 'home.index',
+    'uses' => 'HomeController@index',
+]);
 
 Auth::routes();
 
