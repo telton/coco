300a227da84f6400411587c6d952ef7109e9f9cb
telton <telton@umflint.edu>
2018-01-25 16:21:16 -0500

    Artisan script for importing courses. See #17.

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -7,6 +7,41 @@ use Illuminate\Database\Eloquent\Model;
 class Course extends Model
 {
     /**
+     * Table used by the model.
+     *
+     * @var array
+     */
+    protected $table = 'courses';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'subject',
+        'course_number',
+        'section',
+        'crn',
+        'title',
+        'capacity',
+        'campus',
+        'credits',
+        'semester',
+        'year',
+    ];
+
+    /**
+     * Cast these dates as an instance of Carbon
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
      * Courses users relationship.
      *
      * @author Tyler Elton <telton@umflint.edu>
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,18 @@ namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, SoftDeletes;
+
+    /**
+     * Table used by the model.
+     *
+     * @var array
+     */
+    protected $table = 'users';
 
     /**
      * The attributes that are mass assignable.
