14d65385225ebf5ac0fef7c1784d5e67ebbd5213
telton <telton@umflint.edu>
2018-01-31 13:50:50 -0500

    Add classes to links and parents.

--- app/Http/Menus/CourseMenu.php
+++ app/Http/Menus/CourseMenu.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Menus;
 
+use Spatie\Menu\Link;
+
 class CourseMenu extends BaseMenu
 {
     protected function build($course = null)
@@ -9,5 +11,10 @@ class CourseMenu extends BaseMenu
         $this->addClass('nav flex-column')
             ->linkIf(!is_null($course), route('courses.show', $course->slug), '<span class="nav-item-addon"><i class="fa fa-home"></i></span>Course Home')
             ->linkIf(!is_null($course), '#', '<span class="nav-item-addon"><i class="fa fa-archive"></i></span>Assignments');
+
+        $this->each(function (Link $link) {
+            $link->addParentClass('nav-item');
+            $link->addClass('nav-link');
+        });
     }
 }
