734208d5fa7f3cb2c0c4a3440b452edf32185eb0
telton <telton@umflint.edu>
2018-02-06 09:52:25 -0500

    Adding instructors to the users seeds.

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,56 +14,94 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         if (User::count() == 0) {
-            $role = Role::where('name', 'admin')->firstOrFail();
+            $adminRole = Role::where('name', 'admin')->firstOrFail();
+            $instructorRole = Role::where('name', 'instructor')->firstOrFail();
 
-            // User::create([
-            //     'name'           => 'Admin',
-            //     'email'          => 'admin@admin.com',
-            //     'password'       => bcrypt('password'),
-            //     'remember_token' => str_random(60),
-            //     'role_id'        => $role->id,
-            // ]);
+            // Admins.
             User::create([
                 'name'           => 'Tyler Elton',
                 'email'          => 'telton@umflint.edu',
                 'password'       => bcrypt('password'),
                 'remember_token' => str_random(60),
-                'role_id'        => $role->id,
+                'role_id'        => $adminRole->id,
             ]);
             User::create([
                 'name'           => 'Jason Moehlman',
                 'email'          => 'jmoehlman@umflint.edu',
                 'password'       => bcrypt('password'),
                 'remember_token' => str_random(60),
-                'role_id'        => $role->id,
+                'role_id'        => $adminRole->id,
             ]);
             User::create([
                 'name'           => 'Brandon Krug',
                 'email'          => 'brkrug@umflint.edu',
                 'password'       => bcrypt('password'),
                 'remember_token' => str_random(60),
-                'role_id'        => $role->id,
+                'role_id'        => $adminRole->id,
             ]);
             User::create([
                 'name'           => 'Zach Nelson',
                 'email'          => 'zacharyn@umflint.edu',
                 'password'       => bcrypt('password'),
                 'remember_token' => str_random(60),
-                'role_id'        => $role->id,
+                'role_id'        => $adminRole->id,
             ]);
             User::create([
                 'name'           => 'Gary Landrum',
                 'email'          => 'glandrum@umflint.edu',
                 'password'       => bcrypt('password'),
                 'remember_token' => str_random(60),
-                'role_id'        => $role->id,
+                'role_id'        => $adminRole->id,
             ]);
             User::create([
                 'name'           => 'Mark Allison',
                 'email'          => 'markalli@umflint.edu',
                 'password'       => bcrypt('password'),
                 'remember_token' => str_random(60),
-                'role_id'        => $role->id,
+                'role_id'        => $adminRole->id,
+            ]);
+            // Instructors.
+            User::create([
+                'name'           => 'Murali Mani',
+                'email'          => 'mmani@umflint.edu',
+                'password'       => bcrypt('password'),
+                'remember_token' => str_random(60),
+                'role_id'        => $instructorRole->id,
+            ]);
+            User::create([
+                'name'           => 'Jeffery Livermore',
+                'email'          => 'jefflive@umflint.edu',
+                'password'       => bcrypt('password'),
+                'remember_token' => str_random(60),
+                'role_id'        => $instructorRole->id,
+            ]);
+            User::create([
+                'name'           => 'Vijayaditya Ayyagari',
+                'email'          => 'vayyagar@umflint.edu',
+                'password'       => bcrypt('password'),
+                'remember_token' => str_random(60),
+                'role_id'        => $instructorRole->id,
+            ]);
+            User::create([
+                'name'           => 'Lixing Han',
+                'email'          => 'lxhan@umflint.edu',
+                'password'       => bcrypt('password'),
+                'remember_token' => str_random(60),
+                'role_id'        => $instructorRole->id,
+            ]);
+            User::create([
+                'name'           => 'Ricardo Alfaro',
+                'email'          => 'ralfaro@umflint.edu',
+                'password'       => bcrypt('password'),
+                'remember_token' => str_random(60),
+                'role_id'        => $instructorRole->id,
+            ]);
+            User::create([
+                'name'           => 'Alla Dubrovich',
+                'email'          => 'dubrovic@umflint.edu',
+                'password'       => bcrypt('password'),
+                'remember_token' => str_random(60),
+                'role_id'        => $instructorRole->id,
             ]);
         }
     }
