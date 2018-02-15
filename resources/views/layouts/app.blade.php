b4c735eb62647ee2be296bf01605569592facf56
telton <telton@umflint.edu>
2018-02-15 15:04:42 -0500

    Show app_description in title.
    Gonna be better since the new name is an acronym.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name') }}: {{ env('APP_DESCRIPTION') }}</title>
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ mix('css/app.css') }}" media="all">
