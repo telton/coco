4c0480bdd80c7fc0bdeccc8a4c5dbd251e0c1984
telton <telton@umflint.edu>
2018-03-04 18:59:52 -0500

    SweetAlert2 message on show view as well.

--- public/js/app.js
+++ public/js/app.js
@@ -101075,6 +101075,28 @@ var Viewer = __webpack_require__(275);
             height: '500px',
             initialValue: descriptionValue
         });
+
+        $(this.$refs.delete).on('click', function (e) {
+            // Stop the form from submitting automatically.
+            e.preventDefault();
+            var form = $(this).parents('form');
+
+            // SweetAlert2 popup.
+            swal({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            }).then(function (result) {
+                // If confirm, submit the form.
+                if (result) {
+                    form.submit();
+                }
+            }).catch(swal.noop); // Catch the cancel option so we don't get console errors.
+        });
     }
 });
 
