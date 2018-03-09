e3f114bb9435699408dbe66a590abf3e21b3383b
telton <telton@umflint.edu>
2018-03-09 10:40:12 -0500

    Display assignment submission date.

--- resources/views/courses/grades/index.blade.php
+++ resources/views/courses/grades/index.blade.php
@@ -40,7 +40,9 @@
                             {{ $assignment->due_date->format('m/d/Y') }} at {{ $assignment->due_date->format('h:i A') }}
                         </td>
                         <td>
-                            Date Submitted
+                            @if ($assignment->submission())
+                                {{ $assignment->submission()->created_at->format('m/d/Y') }} at {{ $assignment->submission()->created_at->format('h:i A') }}
+                            @endif
                         </td>
                         <td>
                             My Grade
