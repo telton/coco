e4ee8de47d2a6330bbf1ff93d6db83dd193b3cc5
telton <telton@umflint.edu>
2018-03-09 12:02:58 -0500

    Changed text for grades on CourseMenu.

--- app/Http/Menus/CourseMenu.php
+++ app/Http/Menus/CourseMenu.php
@@ -11,7 +11,7 @@ class CourseMenu extends BaseMenu
         $this->addClass('nav flex-column')
             ->linkIf(!is_null($course), route('courses.show', $course->slug), '<span class="nav-item-addon"><i class="fa fa-home"></i></span>Course Home')
             ->linkIf(!is_null($course), route('courses.assignments.index', $course->slug), '<span class="nav-item-addon"><i class="fa fa-archive"></i></span>Assignments')
-            ->linkIf(!is_null($course), route('courses.grades.index', $course->slug), '<span class="nav-item-addon"><i class="fa fa-calendar-check-o"></i></span>Grades');
+            ->linkIf(!is_null($course), route('courses.grades.index', $course->slug), '<span class="nav-item-addon"><i class="fa fa-calendar-check-o"></i></span>My Grades');
 
         $this->each(function (Link $link) {
             $link->addParentClass('nav-item');
