6a243c7357faae16038ff0e0365d2f7e9facdde0
telton <telton@umflint.edu>
2018-03-09 14:11:33 -0500

    Skeleton for grading dashboard.

---
+++ resources/views/courses/grades/dashboard.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('nav')
+    
+    @include('includes.nav')
+
+@endsection
+
+@section('content')
+
+<div class="card">
+    <div class="card-header">
+        <strong>Grades for:</strong> {{ $course->subject }}{{ $course->course_number }} - {{ sprintf('%02d', $course->section) }}: {{ $course->title }}
+    </div>
+    <div class="card-body">
+        
+    </div>
+</div>
+
+@endsection
+
+
+@section('aside')
+
+    @include('includes.chat')
+
+@endsection
