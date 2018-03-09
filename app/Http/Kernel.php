5e1e05616db9be184e3b6eaab68bc27a6acce037
telton <telton@umflint.edu>
2018-03-09 14:11:14 -0500

    Middleware to check for grading dashboard.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -51,13 +51,14 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-        'auth'                   => \Illuminate\Auth\Middleware\Authenticate::class,
-        'auth.basic'             => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'bindings'               => \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        'can'                    => \Illuminate\Auth\Middleware\Authorize::class,
-        'guest'                  => \App\Http\Middleware\RedirectIfAuthenticated::class,
-        'throttle'               => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'courses.permissions'    => \App\Http\Middleware\Courses\PermissionsCheck::class,
-        'courses.instructor'     => \App\Http\Middleware\Courses\InstructorCheck::class,
+        'auth'                     => \Illuminate\Auth\Middleware\Authenticate::class,
+        'auth.basic'               => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'bindings'                 => \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        'can'                      => \Illuminate\Auth\Middleware\Authorize::class,
+        'guest'                    => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'throttle'                 => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'courses.permissions'      => \App\Http\Middleware\Courses\PermissionsCheck::class,
+        'courses.instructor'       => \App\Http\Middleware\Courses\InstructorCheck::class,
+        'courses.grades.dashboard' => \App\Http\Middleware\Courses\GradesDashboardCheck::class,
     ];
 }
