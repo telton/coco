6df8e45b2c4928b1cd072d9206085b5f0e86eb85
telton <telton@umflint.edu>
2018-03-09 10:13:57 -0500

    Database migration for grades table.

---
+++ database/migrations/2018_03_09_150830_create_grades_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGradesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('grades', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('assignment_id')->unsigned();
+            $table->integer('student_id')->unsigned();
+            $table->integer('grader_id')->unsigned();
+            $table->float('grade');
+            $table->string('letter_grade', 5);
+            $table->boolean('approved')->default(0);
+            $table->timestamps();
+
+            $table->foreign('assignment_id')->references('id')->on('assignments');
+            $table->foreign('student_id')->references('id')->on('users');
+            $table->foreign('grader_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('grades');
+    }
+}
