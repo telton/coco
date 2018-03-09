d5e7ec3cf9bdba89b3b58e54a1b1383963c1ea71
telton <telton@umflint.edu>
2018-03-09 14:11:46 -0500

    Change menu to add grading dashboard.

--- app/Http/Menus/CourseMenu.php
+++ app/Http/Menus/CourseMenu.php
@@ -11,7 +11,8 @@ class CourseMenu extends BaseMenu
         $this->addClass('nav flex-column')
             ->linkIf(!is_null($course), route('courses.show', $course->slug), '<span class="nav-item-addon"><i class="fa fa-home"></i></span>Course Home')
             ->linkIf(!is_null($course), route('courses.assignments.index', $course->slug), '<span class="nav-item-addon"><i class="fa fa-archive"></i></span>Assignments')
-            ->linkIf(!is_null($course), route('courses.grades.index', $course->slug), '<span class="nav-item-addon"><i class="fa fa-calendar-check-o"></i></span>My Grades');
+            ->linkIf((!is_null($course) && \Illuminate\Support\Facades\Auth::user()->hasRole('student')), route('courses.grades.index', $course->slug), '<span class="nav-item-addon"><i class="fa fa-calendar-check-o"></i></span>My Grades')
+            ->linkIf((!is_null($course) && \Illuminate\Support\Facades\Auth::user()->hasRole(['admin', 'instructor', 'grader'])), route('courses.grades.dashboard', $course->slug), '<span class="nav-item-addon"><i class="fa fa-calendar-check-o"></i></span>Grades');
 
         $this->each(function (Link $link) {
             $link->addParentClass('nav-item');
