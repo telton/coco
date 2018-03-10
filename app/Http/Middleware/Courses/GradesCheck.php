4dd2eb34ab872e064737175a4f0a07f299a99683
telton <telton@umflint.edu>
2018-03-10 14:11:35 -0500

    Logic for grades middleware.
    Because if I didn't implement it now, I'd forget completely.

--- app/Http/Middleware/Courses/GradesCheck.php
+++ app/Http/Middleware/Courses/GradesCheck.php
@@ -3,9 +3,27 @@
 namespace App\Http\Middleware\Courses;
 
 use Closure;
+use Illuminate\Contracts\Auth\Guard;
 
 class GradesCheck
 {
+    /**
+     * @var Guard
+     */
+    protected $auth;
+
+    /**
+     * GradesCheck constructor.
+     *
+     * @author Tyler Elton <telton@umflint.edu>
+     *
+     * @param Guard $auth
+     */
+    public function __construct(Guard $auth)
+    {
+        $this->auth = $auth;
+    }
+
     /**
      * Handle an incoming request.
      *
@@ -15,6 +33,16 @@ class GradesCheck
      */
     public function handle($request, Closure $next)
     {
+        // Make sure the user is logged in.
+        if (!$this->auth->check()) {
+            return redirect()->guest('/login');
+        }
+
+        // If the user is not an admin, instructor or grader, don't allow them access.
+        if (!$this->auth->user()->hasRole(['admin', 'instructor', 'grader'])) {
+            abort(403, 'You do not have access to this resource.');
+        }
+
         return $next($request);
     }
 }
