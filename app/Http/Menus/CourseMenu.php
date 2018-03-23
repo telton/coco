a71e7060ab40fd589f404b06bc066fa85cb25745
telton <telton@umflint.edu>
2018-03-23 11:13:22 -0400

    Add a link in CourseMenu for notes index.

--- app/Http/Menus/CourseMenu.php
+++ app/Http/Menus/CourseMenu.php
@@ -11,6 +11,7 @@ class CourseMenu extends BaseMenu
         $this->addClass('nav flex-column')
             ->linkIf(!is_null($course), route('courses.show', $course->slug), '<span class="nav-item-addon"><i class="fa fa-home"></i></span>Course Home')
             ->linkIf(!is_null($course), route('courses.assignments.index', $course->slug), '<span class="nav-item-addon"><i class="fa fa-archive"></i></span>Assignments')
+            ->linkIf(!is_null($course), route('courses.notes.index', $course->slug), '<span class="nav-item-addon"><i class="fa fa-edit"></i></span>My Notes')
             ->linkIf((!is_null($course) && \Illuminate\Support\Facades\Auth::user()->hasRole('student')), route('courses.grades.index', $course->slug), '<span class="nav-item-addon"><i class="fa fa-calendar-check-o"></i></span>My Grades')
             ->linkIf((!is_null($course) && \Illuminate\Support\Facades\Auth::user()->hasRole(['admin', 'instructor', 'grader'])), route('courses.grades.dashboard', $course->slug), '<span class="nav-item-addon"><i class="fa fa-calendar-check-o"></i></span>Grades');
 
