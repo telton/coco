ab154d4c830dd544657913d9354c3e4aa025b062
telton <telton@umflint.edu>
2018-03-26 16:45:37 -0400

    Forgot the homepage.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,36 +2,38 @@
 
 @section('content')
 
-<div class="card card-center">
-    <div class="card-header">
-        My Courses
-    </div>
-    <div class="card-body courses-list">
-        <table class="table table-striped">
-            @if(count($courses) !== 0)
-                <p><strong>Courses for which you are:</strong> {{ ucfirst(Auth::user()->role->name) }}</p>
-                @foreach($courses as $course)
+<div class="col-lg-6 offset-lg-3" style="margin-top: 20px;">
+    <div class="card">
+        <div class="card-header">
+            My Courses
+        </div>
+        <div class="card-body courses-list">
+            <table class="table table-striped">
+                @if(count($courses) !== 0)
+                    <p><strong>Courses for which you are:</strong> {{ ucfirst(Auth::user()->role->name) }}</p>
+                    @foreach($courses as $course)
+                        <tr>
+                            <td>
+                                <a href="{{ route('courses.show', $course->slug) }}" class="course-link">
+                                    {{ $course->subject }}{{ $course->course_number }} - {{ sprintf('%02d', $course->section) }}: {{ $course->title }}
+                                </a>
+                            </td>
+                            @if (!Auth::user()->hasRole('instructor'))
+                                <td>
+                                    <strong>Instructor:</strong> {{ $course->instructor->name }}
+                                </td>
+                            @endif
+                        </tr>
+                    @endforeach
+                @else
                     <tr>
                         <td>
-                            <a href="{{ route('courses.show', $course->slug) }}" class="course-link">
-                                {{ $course->subject }}{{ $course->course_number }} - {{ sprintf('%02d', $course->section) }}: {{ $course->title }}
-                            </a>
+                            <p>You are not registered for any courses!</p>
                         </td>
-                        @if (!Auth::user()->hasRole('instructor'))
-                            <td>
-                                <strong>Instructor:</strong> {{ $course->instructor->name }}
-                            </td>
-                        @endif
                     </tr>
-                @endforeach
-            @else
-                <tr>
-                    <td>
-                        <p>You are not registered for any courses!</p>
-                    </td>
-                </tr>
-            @endif
-        </table>
+                @endif
+            </table>
+        </div>
     </div>
 </div>
 
