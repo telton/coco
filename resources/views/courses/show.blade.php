b7442681fd69daa9458ac61ab68990525c584c6d
Tyler Elton <telton@umflint.edu>
2018-04-09 21:49:29 +0000

    Helper text under description.

--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -18,6 +18,8 @@
         @else
             <span class="alert alert-info flex-row-center">There isn't a description for this course!</span>
         @endif
+        
+        <p style="margin-top: 2em; text-align:center;"><strong>Use the navigation panel to view the course assignments, your notes, grades, or access chat.</strong></p>
     </div>
 </div>
 
