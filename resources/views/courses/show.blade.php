7e07e1e375b8f484a9772368957ff8604ad01c1e
telton <telton@umflint.edu>
2018-04-09 17:54:41 -0400

    Alert.

--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -18,8 +18,8 @@
         @else
             <span class="alert alert-info flex-row-center">There isn't a description for this course!</span>
         @endif
-        
-        <p style="margin-top: 2em; text-align:center;"><strong>Use the navigation panel to view the course assignments, your notes, grades, or access chat.</strong></p>
+
+        <p style="margin-top: 2em; text-align:center;" class="alert alert-warning"><strong>Use the navigation panel to view the course assignments, your notes, grades, or access chat.</strong></p>
     </div>
 </div>
 
